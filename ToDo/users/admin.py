07392ec269b2ba96246f2ac6df1256466c8374a8
Kuznetsov-Serg <ksn1974@mail.ru>
2021-10-31 14:48:16 +0300

    Lesson 3.01 (Введение в REST и Django REST Framework) 1.	Создать новый проект на github или gitlab. 2.	Создать django-проект. 3.	Установить DRF и подключить его к django-проекту. 4.	Создать приложение для работы с пользователем. 5.	Создать свою модель пользователя. 6.	В ней поле email сделать уникальным. 7.	Сделать для неё базовое API — по аналогии модели Author. В качестве полей выбрать username, firstname, lastname, email. Если выбрать все поля, при попытке сериализации может возникнуть ошибка сериализации связанного поля. Эту тему мы рассмотрим далее. 8.	Подключить стандартную админку. 9.	Создать суперпользователя. 10.	* Создать management command — скрипт для запуска через manage.py для автоматического создания суперпользователя и нескольких тестовых пользователей (Management commands).

---
+++ ToDo/users/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from .models import User
+
+# admin.site.register(ShopUser)
+@admin.register(User)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ("username", "first_name", "last_name", "email", "password")
+    list_filter = ('is_active', 'is_staff', 'is_superuser')
+    # list_filter = ('created', 'user')
+
+# admin.site.register(User)
---
+++ ToDo/users/models.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.db import models
+
+# class ShopUser(AbstractUser):
+#     avatar = models.ImageField(upload_to='users_avatars', max_length=256, blank=True)
+
+class User(AbstractUser):
+    # avatar = models.ImageField(upload_to='users_avatars', max_length=256, blank=True)
+    class Meta(object):
+        unique_together = ('email',)    # делает уникальным
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
+
+
+@receiver(pre_save, sender=User)
+def user_pre_save(sender, **kwargs):
+    email = kwargs['instance'].email
+    username = kwargs['instance'].username
+
+    if not email:
+        raise ValidationError("email required")
+    if sender.objects.filter(email=email).exclude(username=username).count():
+        raise ValidationError("email needs to be unique")
+
+
