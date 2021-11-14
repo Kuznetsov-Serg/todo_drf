becec2568163101784afa3df072b08553dd2312e
Kuznetsov-Serg <ksn1974@mail.ru>
2021-11-14 16:29:44 +0300

    Revert "4.	(Задание со *) Добавить в проект авторизацию по JWT токену: ссылка."
    This reverts commit 24a55cbe0ffa658a57dfd421ed57fe5f6edb3cc1.

--- ToDo/todo/settings.py
+++ ToDo/todo/settings.py
@@ -12,10 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
-from datetime import timedelta      # для JSON Web Token (сокр. JWT)
-from django.conf import settings    # для JWT
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -49,8 +45,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'rest_framework.authtoken',
-    'rest_framework_simplejwt',                     # JWT
-    'rest_framework_simplejwt.token_blacklist',     # JWT
     'django_filters',
 
     'users',
@@ -173,44 +167,8 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
-    # Для JWT
-    'EXCEPTION_HANDLER': 'project.exceptions.core_exception_handler',
-    'NON_FIELD_ERRORS_KEY': 'error',
 }
 # from rest_framework.permissions import IsAuthenticated
 # в режиме отладки добавим интерфейс удобного API-редактирования
 if DEBUG:
     REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'].append('rest_framework.renderers.BrowsableAPIRenderer')
-
-# для JWT
-SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    'ROTATE_REFRESH_TOKENS': False,
-    'BLACKLIST_AFTER_ROTATION': False,
-    'UPDATE_LAST_LOGIN': False,
-
-    'ALGORITHM': 'HS256',
-    'SIGNING_KEY': settings.SECRET_KEY,
-    'VERIFYING_KEY': None,
-    'AUDIENCE': None,
-    'ISSUER': None,
-    'JWK_URL': None,
-    'LEEWAY': 0,
-
-    'AUTH_HEADER_TYPES': ('Bearer',),
-    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
-    'USER_ID_FIELD': 'id',
-    'USER_ID_CLAIM': 'user_id',
-    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
-
-    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
-    'TOKEN_TYPE_CLAIM': 'token_type',
-    'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
-
-    'JTI_CLAIM': 'jti',
-
-    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
-    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
-    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
-}
--- ToDo/todo/urls.py
+++ ToDo/todo/urls.py
@@ -33,6 +33,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
     path('test_views/', include('test_views.urls', namespace='test_views'), name='test_views'),
-    path('api-token-auth/', obtain_auth_token),
-    path('jwt/', include('users.urls', namespace='authentication')),  # для JWT
+    path('api-token-auth/', obtain_auth_token)
 ]
